ae96afceb43cdcf742321bbe5c049e9e4633662e
Enzo Innocenzi <enzo@innocenzi.dev>
2020-06-21 08:05:00 +0200

    refactor: expose api by default

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,14 @@
 import { GeneratorContract } from './Contracts';
+import { Log } from './Logger';
 
 export const Preset = {
   make: (preset: GeneratorContract) => preset as GeneratorContract,
 };
 
-export { Log } from '@/Logger';
+export const Logger = {
+  info: (message: string, ...args: string[]) => Log.info(message, ...args) as any,
+  success: (message: string, ...args: string[]) => Log.success(message, ...args) as any,
+  fatal: (message: string, ...args: string[]) => Log.fatal(message, ...args) as any,
+};
+
 export { flags } from '@oclif/parser';
