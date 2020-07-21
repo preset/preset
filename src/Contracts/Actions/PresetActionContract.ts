82696fd77b2e1aa786895fe65a5cde242f7d32d5
Enzo Innocenzi <enzo@innocenzi.dev>
2020-07-21 06:56:12 +0200

    feat: arguments for preset action (#30)

--- src/Contracts/Actions/PresetActionContract.ts
+++ src/Contracts/Actions/PresetActionContract.ts
@@ -8,4 +8,14 @@ export interface PresetActionContract extends BaseActionContract<'preset'> {
    * Any string that can be resolved to a preset.
    */
   preset: string;
+
+  /**
+   * Arguments to pass to the preset.
+   */
+  arguments?: string | string[];
+
+  /**
+   * Inherits current command line arguments.
+   */
+  inherit?: boolean;
 }
