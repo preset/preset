ac329721fa134f3d00a1fc1f413f3d3277204ef6
Enzo Innocenzi <enzo@innocenzi.dev>
2020-07-24 17:10:03 +0200

    feat: handle mono-repositories (#38)

--- src/Resolvers/GithubResolver.ts
+++ src/Resolvers/GithubResolver.ts
@@ -1,33 +1,54 @@
 import { injectable } from 'inversify';
 import { ResolverResultContract } from '@/Contracts';
-import { GitResolver } from './GitResolver';
+import { GitResolver, GitResolverResult } from './GitResolver';
 import { Name } from '@/Container';
+import { Logger } from '@/Logger';
 
 /**
  * A resolver that clones a Github repository and returns its local, temporary path.
  */
 @injectable()
 export class GithubResolver extends GitResolver {
+  public readonly urlRegex = /^(?:(?:https:\/\/)|(?:git@))(?:(?:www\.)?github\.com)[\/|:]([\w-]+)\/([\w-]+)(?:\.git)?(?:\:([\w-\/]+))?$/;
   public readonly name: string = Name.GithubResolver;
 
   async resolve(input: string): Promise<ResolverResultContract> {
-    const [matches, org, name] =
-      input.match(
-        /^(?:(?:(?:https?|git)(?:@|\:\/\/)(?:www\.)?github\.com(?::|\/))?(?:([\w-]+)\/)?([\w-]+))(?:\.git)?$/
-      ) ?? [];
+    const data = this.resolveUsePreset(input) || this.resolveShortSyntax(input) || this.resolveUrlSyntax(input);
 
-    if (!matches) {
-      return {
-        success: false,
-      };
+    if (!data) {
+      return { success: false };
     }
 
-    const repositoryUrl = this.getRepositoryUrl(org ?? 'use-preset', name);
-
-    return this.clone(repositoryUrl);
+    return this.clone(data);
   }
 
-  private getRepositoryUrl(organizationOrUser: string, repository: string): string {
-    return `git://github.com/${organizationOrUser}/${repository}.git`;
+  resolveUsePreset(input: string): GitResolverResult | false {
+    Logger.info(`Trying to resolve the use-preset short syntax.`);
+    const [matches, repository, path] = input.match(/^([\w-]+)(?:\:+([\w-\/]+))?$/) ?? [];
+
+    if (!matches) {
+      return false;
+    }
+
+    return {
+      organization: 'use-preset',
+      repository,
+      path: path ?? '/',
+    };
+  }
+
+  resolveShortSyntax(input: string): GitResolverResult | false {
+    Logger.info(`Trying to resolve the GitHub short syntax.`);
+    const [matches, organization, repository, path] = input.match(/^([\w-]+)\/([\w-]+)(?:\:+([\w-\/]+))?$/) ?? [];
+
+    if (!matches) {
+      return false;
+    }
+
+    return {
+      organization,
+      repository,
+      path: path ?? '/',
+    };
   }
 }
