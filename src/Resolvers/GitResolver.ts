a71286d8f4012eac9b42efd408666e95d2e960c6
Enzo Innocenzi <enzo@innocenzi.dev>
2020-07-24 16:54:31 +0200

    feat: handle mono-repositories

--- src/Resolvers/GitResolver.ts
+++ src/Resolvers/GitResolver.ts
@@ -4,31 +4,64 @@ import { Logger } from '@/Logger';
 import { Name } from '@/Container';
 import git from 'simple-git';
 import tmp from 'tmp';
+import path from 'path';
+
+export interface GitResolverResult {
+  organization: string;
+  repository: string;
+  path: string;
+}
 
 /**
  * A resolver that clones a git repository and returns its local, temporary path.
  */
 @injectable()
 export class GitResolver implements ResolverContract {
+  public readonly urlRegex = /^(?:(?:https:\/\/)|(?:git@))(?:[\w\.]+)[\/|:]([\w-]+)\/([\w-]+)(?:\.git)?(?:\:([\w-\/]+))?$/;
   public readonly name: string = Name.GitResolver;
   public readonly prefix: string = 'git::';
 
   async resolve(input: string): Promise<ResolverResultContract> {
     if (!input.startsWith(this.prefix)) {
-      return {
-        success: false,
-      };
+      return { success: false };
     }
 
     const repositoryUrl = input.substr(this.prefix.length);
+    const data = this.resolveUrlSyntax(repositoryUrl);
 
-    return this.clone(repositoryUrl);
+    if (!data) {
+      Logger.info(`Received the git prefix, but the URL after was not valid.`);
+      return { success: false };
+    }
+
+    return this.clone(data);
+  }
+
+  protected resolveUrlSyntax(input: string): GitResolverResult | false {
+    Logger.info(`Trying to resolve the full url syntax.`);
+    const [matches, organization, repository, path] = input.match(this.urlRegex) ?? [];
+
+    if (!matches) {
+      return false;
+    }
+
+    return {
+      organization,
+      repository,
+      path: path ?? '/',
+    };
+  }
+
+  protected getRepositoryUrl({ organization, repository }: GitResolverResult): string {
+    return `git@github.com:${organization}/${repository}.git`;
   }
 
   /**
    * Clones the given repository.
    */
-  protected async clone(repositoryUrl: string): Promise<ResolverResultContract> {
+  protected async clone(repositoryData: GitResolverResult): Promise<ResolverResultContract> {
+    const repositoryUrl = this.getRepositoryUrl(repositoryData);
+
     try {
       Logger.info(`Generating temporary directory to clone ${repositoryUrl} into.`);
       const temporary = tmp.dirSync();
@@ -40,7 +73,7 @@ export class GitResolver implements ResolverContract {
 
       return {
         success: true,
-        path: temporary.name,
+        path: path.join(temporary.name, repositoryData.path),
         temporary: true,
       };
     } catch (error) {
