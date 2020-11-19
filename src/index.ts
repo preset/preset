3271ba7f682eac68044ae4a93d842c333269781f
Enzo Innocenzi <enzo@innocenzi.dev>
2020-11-19 21:49:05 +0100

    fix: use an exit code

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,6 @@ container
   .catch((error) => {
     logger.fatal('An uncaught error has occured. This should not happen, so feel free to report an issue.');
     logger.fatal(error);
-  });
+    process.exitCode = 1;
+  })
+  .then((code) => (process.exitCode = code || 0));
