902a4bc473717e67f20317b7f597c6dc7e6096f0
Enzo Innocenzi <enzo@innocenzi.dev>
2021-12-31 16:49:56 +0100

    refactor(import): use vm instead of bundle-require

--- packages/core/src/import.ts
+++ packages/core/src/import.ts
@@ -1,19 +1,139 @@
-import { bundleRequire } from 'bundle-require'
+import vm from 'vm'
+import fs from 'fs'
+import path from 'path'
+import { buildSync } from 'esbuild'
+import { Preset } from './types'
 import { debug } from './utils'
+import * as preset from './index'
 
 /**
- * Imports the preset file.
- *
- * @param filepath Absolute path to the file.
+ * Imports the given preset file.
+ * @param filepath
+ * @returns
  */
 export async function importPresetFile(filepath: string) {
-	debug.import(`Importing "${filepath}" with bundle-require."`)
+	debug.import(`Importing ${filepath}}.`)
 
-	// https://github.com/antfu/local-pkg
-	const result = await bundleRequire({
-		filepath,
-		require: (id: string) => import(id),
+	const script = fs.readFileSync(filepath).toString()
+	const sanitizedScript = removeSelfImportStatement(script)
+
+	return await evaluateConfiguration(sanitizedScript, path.dirname(filepath), filepath)
+}
+
+/**
+ * Evaluates the configuration and returns the preset.
+ */
+async function evaluateConfiguration(script: string, directory: string, filepath: string) {
+	const context = vm.createContext(createContext(directory, filepath))
+
+	const code = transformScript(script, directory, filepath)
+	vm.runInContext(code, context)
+
+	const defaultExportKey = Object.keys(context).find((value) => value.endsWith('_default'))
+
+	if (defaultExportKey) {
+		const result = context[defaultExportKey] as Preset
+
+		if (!result.name || !result.apply) {
+			throw new Error('Preset configuration is missing a name or an apply handler.')
+		}
+
+		debug.import(`Found default export "${defaultExportKey}":`, result.name)
+
+		return result
+	}
+
+	throw new Error('Preset configuration is missing a default export.')
+}
+
+/**
+ * Removes the import statements that concerns the core preset package.
+ */
+function removeSelfImportStatement(script: string) {
+	debug.import('Removing "import" statements that import the core.')
+
+	return script
+		.split(/\r\n|\r|\n/)
+		.filter((line) => {
+			const lineImports = ['import', 'require'].some((statement) => line.includes(statement))
+			const lineMentionsImportValue = ['@preset/core', ...Object.keys(preset)].some((imp) => line.includes(imp))
+
+			if (lineImports && lineMentionsImportValue) {
+				return false
+			}
+
+			return true
+		})
+		.join('\n')
+}
+
+/**
+ * Transforms the raw script with esbuild so it can be imported in a VM.
+ */
+function transformScript(contents: string, resolveDir: string, sourcefile: string): string {
+	debug.import('Transforming script with esbuild.')
+
+	const { outputFiles } = buildSync({
+		stdin: {
+			contents,
+			resolveDir,
+			sourcefile,
+			loader: 'ts',
+		},
+		platform: 'node',
+		format: 'cjs',
+		external: ['@preset/core'],
+		bundle: true,
+		write: false,
 	})
 
-	return result.mod.default ?? result.mod
+	return outputFiles[0].text
+}
+
+/**
+ * Creates a VM context.
+ */
+function createContext(directory: string, filename: string): Record<string, any> {
+	debug.import('Creating VM context.')
+
+	const exports = {}
+	const moduleGlobals = {
+		exports,
+		require,
+		module: {
+			exports,
+			filename,
+			id: filename,
+			path: directory,
+			require: module.require,
+		},
+		__dirname: directory,
+		__filename: filename,
+	}
+
+	const nodeGlobals = {
+		Buffer,
+		clearImmediate,
+		clearInterval,
+		clearTimeout,
+		console,
+		global,
+		process,
+		queueMicrotask,
+		setImmediate,
+		setInterval,
+		setTimeout,
+		TextDecoder: (globalThis as any).TextDecorator,
+		TextEncoder: (globalThis as any).TextEncoder,
+		URL: (globalThis as any).URL,
+		URLSearchParams: (globalThis as any).URLSearchParams,
+		WebAssembly: (globalThis as any).WebAssembly,
+	}
+
+	return {
+		...globalThis,
+		...moduleGlobals,
+		...nodeGlobals,
+		...preset,
+	}
 }
