4e819d1e3493751f43dc45b8447000a2fd3dd54c
Enzo Innocenzi <enzo@innocenzi.dev>
2022-01-07 21:17:22 +0100

    test: cover `apply-nested-preset`

---
+++ packages/core/test/actions/apply-nested-preset.test.ts
@@ -0,0 +1,67 @@
+import { it, expect } from 'vitest'
+import { applyNestedPreset, emitter, PresetContext } from '../../src'
+import { usingSandbox, presetFixture } from '../utils'
+
+it('applies the given nested preset', async() => await usingSandbox({
+	fn: async(_, makeTestPreset) => {
+		const result: any = {}
+		const { executePreset } = await makeTestPreset({
+			handler: async() => await applyNestedPreset({
+				resolvable: presetFixture('basic-preset.ts'),
+			}),
+		})
+
+		emitter.on('preset:start', (context) => result.context = context)
+
+		await executePreset()
+
+		expect(result.context.name).toBe('basic-preset')
+	},
+}))
+
+it('can be given arguments', async() => await usingSandbox({
+	fn: async(_, makeTestPreset) => {
+		const result = { context: undefined as PresetContext | undefined }
+		const { executePreset } = await makeTestPreset({
+			handler: async() => await applyNestedPreset({
+				args: ['some-arg', '--some-flag'],
+				resolvable: presetFixture('basic-preset.ts'),
+			}),
+		})
+
+		emitter.on('preset:start', (context) => result.context = context)
+
+		await executePreset()
+
+		expect(result.context?.name).toBe('basic-preset')
+		expect(result.context?.args).toStrictEqual(['some-arg'])
+		expect(result.context?.options).toMatchObject({ someFlag: true })
+		expect(result.context?.applyOptions.args).toStrictEqual(['some-arg', '--some-flag'])
+	},
+}))
+
+it('can inherit arguments', async() => await usingSandbox({
+	fn: async(_, makeTestPreset) => {
+		const result = { context: undefined as PresetContext | undefined }
+		const { executePreset } = await makeTestPreset({
+			handler: async() => await applyNestedPreset({
+				inheritsArguments: true,
+				resolvable: presetFixture('basic-preset.ts'),
+			}),
+		}, {
+			args: ['some-arg', 'some arg with spaces', '--some-flag', '--some-other-flag', 'the flag value'],
+		})
+
+		emitter.on('preset:start', (context) => result.context = context)
+
+		await executePreset()
+
+		expect(result.context?.name).toBe('basic-preset')
+		expect(result.context?.applyOptions.args).toStrictEqual(['some-arg', 'some arg with spaces', '--some-flag', '--some-other-flag', 'the flag value'])
+		expect(result.context?.args).toStrictEqual(['some-arg', 'some arg with spaces'])
+		expect(result.context?.options).toMatchObject({
+			someFlag: true,
+			someOtherFlag: 'the flag value',
+		})
+	},
+}))
