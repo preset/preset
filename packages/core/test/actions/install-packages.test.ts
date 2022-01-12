1d84dfbfb983c847537069f2277a2b189cb0b32c
Enzo Innocenzi <enzo@innocenzi.dev>
2022-01-12 22:27:19 +0100

    test(install-packages): ensure multiple packages can be added

--- packages/core/test/actions/install-packages.test.ts
+++ packages/core/test/actions/install-packages.test.ts
@@ -140,3 +140,31 @@ it('install the given node packages with the specified package manager', async()
 		'.npmrc': { type: 'file', content: 'shared-workspace-lockfile=false' },
 	}, // necessary so it's not installed in the first package.json directory
 }))
+
+it('install the given multiple packages at once', async() => await usingSandbox({
+	fn: async({ targetDirectory }, makeTestPreset) => {
+		const { executePreset } = await makeTestPreset({
+			handler: async() => await installPackages({
+				for: 'node',
+				install: ['debug@^4.3.3', 'picocolors@^1.0.0'],
+			}),
+		})
+
+		await executePreset()
+		await expectStructureMatches(targetDirectory, {
+			'node_modules': { type: 'directory' },
+			'node_modules/debug': { type: 'directory' },
+			'package-lock.json': { type: 'file' },
+			'package.json': {
+				type: 'file',
+				json: {
+					dependencies: {
+						debug: '^4.3.3',
+						picocolors: '^1.0.0',
+					},
+				},
+			},
+		})
+	},
+	targetStructure: { 'package.json': { type: 'file', content: '{}' } }, // necessary so it's not installed in the first package.json directory
+}))
