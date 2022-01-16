d163d5d78095626601ecde8ce919f265fe9bf6a2
Enzo Innocenzi <enzo@innocenzi.dev>
2022-01-16 23:46:06 +0100

    test(group): ensure action fails when sub-actions fail

--- packages/core/test/actions/group.test.ts
+++ packages/core/test/actions/group.test.ts
@@ -1,5 +1,5 @@
 import { it, expect } from 'vitest'
-import { group, extractTemplates, emitter, ActionContext } from '../../src'
+import { group, extractTemplates, executeCommand, emitter, ActionContext } from '../../src'
 import { usingSandbox } from '../utils'
 
 it('applies actions in the group', async() => await usingSandbox({
@@ -15,9 +15,8 @@ it('applies actions in the group', async() => await usingSandbox({
 
 		emitter.on('action:start', (context) => actions.push(context))
 
-		await executePreset()
-
-		expect(actions[0]).toMatchObject({ groupContextId: undefined, name: 'group' })
+		expect(await executePreset()).toBe(true)
+		expect(actions[0]).toMatchObject({ groupContextId: undefined, name: 'group', status: 'applied' })
 		expect(actions[1]).toMatchObject({ groupContextId: actions[0].id, name: 'extract-templates' })
 	},
 }))
@@ -45,11 +44,30 @@ it('does not mix multiple groups', async() => await usingSandbox({
 
 		emitter.on('action:start', (context) => actions.push(context))
 
-		await executePreset()
-
-		expect(actions[0]).toMatchObject({ groupContextId: undefined, title: 'group 1' })
+		expect(await executePreset()).toBe(true)
+		expect(actions[0]).toMatchObject({ groupContextId: undefined, title: 'group 1', status: 'applied' })
 		expect(actions[1]).toMatchObject({ groupContextId: actions[0].id, name: 'extract-templates' })
-		expect(actions[2]).toMatchObject({ groupContextId: undefined, title: 'group 2' })
+		expect(actions[2]).toMatchObject({ groupContextId: undefined, title: 'group 2', status: 'applied' })
 		expect(actions[3]).toMatchObject({ groupContextId: actions[2].id, name: 'extract-templates' })
+	},
+}))
+
+it('fails when sub-action fail', async() => await usingSandbox({
+	fn: async(_, makeTestPreset) => {
+		const actions: ActionContext[] = []
+		const { executePreset } = await makeTestPreset({
+			handler: async() => await group({
+				handler: async() => {
+					await executeCommand({ command: 'me-no-exist' })
+				},
+			}),
+		})
+
+		emitter.on('action:start', (context) => actions.push(context))
+
+		expect(await executePreset()).toBe(false)
+		expect(actions[0]).toMatchObject({ groupContextId: undefined, name: 'group', status: 'failed' })
+		expect(actions[1]).toMatchObject({ groupContextId: actions[0].id, name: 'execute-command' })
+		expect(actions[0].error).toBeDefined()
 	},
 }))
