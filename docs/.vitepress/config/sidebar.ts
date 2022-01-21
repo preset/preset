edd61f4dcf7060f6681491c95643c17b79aeb413
Enzo Innocenzi <enzo@innocenzi.dev>
2022-01-21 16:50:46 +0100

    docs: remove index redirect

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -18,7 +18,7 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
 			children: [
 				{
 					text: 'Introduction',
-					link: '/basics/introduction',
+					link: '/',
 				},
 				{
 					text: 'Getting started',
