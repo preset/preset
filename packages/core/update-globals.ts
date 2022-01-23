64f6f403775b8babccc1d9e690bf501c5dc4a363
Enzo Innocenzi <enzo@innocenzi.dev>
2022-01-23 18:37:31 +0100

    dx: automatically update `globals.d.ts`

---
+++ packages/core/update-globals.ts
@@ -0,0 +1,29 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import * as actions from './src/actions'
+
+const globalsFile = path.resolve(__dirname, './globals.d.ts')
+
+function erase() {
+	fs.unlinkSync(globalsFile)
+}
+
+function write() {
+	const actionImports = Object.keys(actions)
+		.filter((name) => !['default'].includes(name))
+		.map((name) => `const ${name}: typeof import('@preset/core')['${name}']`)
+
+	const globals = `declare global {
+		const definePreset: typeof import('@preset/core')['definePreset']
+		${actionImports.join('\n	')}
+	}
+	export {}\n`
+
+	fs.writeFileSync(globalsFile, globals)
+}
+
+if (process.argv.includes('--delete')) {
+	erase()
+} else {
+	write()
+}
