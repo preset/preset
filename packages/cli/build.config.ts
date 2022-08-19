e1600152b4d54204b2e473da22c3a76a601ea385
Enzo Innocenzi <enzo@innocenzi.dev>
2022-08-19 17:12:18 +0200

    build: remove warnings

--- packages/cli/build.config.ts
+++ packages/cli/build.config.ts
@@ -3,6 +3,7 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
 	entries: ['./src/cli'],
 	clean: true,
+	failOnWarn: false,
 	rollup: {
 		cjsBridge: true,
 	},
--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -11,5 +11,6 @@ export default defineBuildConfig({
 	rollup: {
 		emitCJS: true,
 		cjsBridge: true,
+		inlineDependencies: true,
 	},
 })
