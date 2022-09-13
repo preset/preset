ee48ab5d657a9fdf9c7a99890c777e88eaae440d
Enzo Innocenzi <enzo@innocenzi.dev>
2022-09-13 10:25:10 +0200

    fix(cli): exit with code 1 when preset fails in debug mode

--- packages/cli/src/reporters/debug.ts
+++ packages/cli/src/reporters/debug.ts
@@ -1,7 +1,20 @@
 import _debug from 'debug'
+import { emitter } from '@preset/core'
 import { makeReporter } from '../types'
 
 export default makeReporter({
 	name: 'debug',
-	registerEvents: () => _debug.enable('preset:*'),
+	registerEvents: () => {
+		_debug.enable('preset:*')
+		const failedPresets: Set<string> = new Set([])
+		emitter.on('preset:end', (context) => {
+			if (context.status === 'failed') {
+				failedPresets.add(context.id)
+			}
+
+			if (context.count === 0 && failedPresets.size > 0) {
+				process.exit(1)
+			}
+		})
+	},
 })
