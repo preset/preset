a1508bbf74885ef1323eedfba83b77e6a5b2a55e
Enzo Innocenzi <enzo@innocenzi.dev>
2026-02-16 12:05:27 +0100

    test: remove pnpm tests

--- packages/core/test/actions/install-packages.test.ts
+++ packages/core/test/actions/install-packages.test.ts
@@ -154,37 +154,6 @@ it('installs the given node package with yarn when yarn was used to run the pres
 		targetStructure: { 'package.json': { type: 'file', content: '{}' } },
 	}))
 
-it.todo('installs the given node package with pnpm when pnpm was used to run the preset', async () =>
-	await usingSandbox({
-		fn: async ({ targetDirectory }, makeTestPreset) => {
-			process.env.npm_config_user_agent = 'pnpm'
-			const { executePreset } = await makeTestPreset({
-				handler: async () =>
-					await installPackages({
-						for: 'node',
-						packages: 'debug@^4.3.4',
-						additionalArgs: ['--ignore-workspace'],
-					}),
-			})
-
-			await executePreset()
-			await expectStructureMatches(targetDirectory, {
-				'node_modules': { type: 'directory' },
-				'node_modules/debug': { type: 'directory' },
-				'pnpm-lock.yaml': { type: 'file' },
-				'package.json': {
-					type: 'file',
-					json: {
-						dependencies: {
-							debug: '^4.3.4',
-						},
-					},
-				},
-			})
-		},
-		targetStructure: { 'package.json': { type: 'file', content: '{}' } },
-	}))
-
 it('installs the given node package as development dependencies', async () =>
 	await usingSandbox({
 		fn: async ({ targetDirectory }, makeTestPreset) => {
@@ -214,37 +183,6 @@ it('installs the given node package as development dependencies', async () =>
 			})
 		},
 		targetStructure: { 'package.json': { type: 'file', content: '{}' } },
-	}))
-
-it('installs the given node package with the specified package manager', async () =>
-	await usingSandbox({
-		fn: async ({ targetDirectory }, makeTestPreset) => {
-			const { executePreset } = await makeTestPreset({
-				handler: async () =>
-					await installPackages({
-						for: 'node',
-						packages: 'debug@4.3.3',
-						packageManager: 'pnpm',
-						additionalArgs: ['--ignore-workspace'],
-					}),
-			})
-
-			await executePreset()
-			await expectStructureMatches(targetDirectory, {
-				'node_modules': { type: 'directory' },
-				'node_modules/debug': { type: 'directory' },
-				'pnpm-lock.yaml': { type: 'file' },
-				'package.json': {
-					type: 'file',
-					json: {
-						dependencies: {
-							debug: '4.3.3',
-						},
-					},
-				},
-			})
-		},
-		targetStructure: emptyPackageJsonStructure,
 	}))
 
 it('installs the given packages at once', async () =>
@@ -463,31 +401,6 @@ it('installs existing packages with yarn when yarn was used to run the preset', 
 				'node_modules': { type: 'directory' },
 				'node_modules/debug': { type: 'directory' },
 				'yarn.lock': { type: 'file' },
-			})
-		},
-		targetStructure: {
-			...emptyPackageJsonStructure,
-			'package.json': { type: 'file', content: '{"dependencies": {"debug": "^4.3.4"}}' },
-		},
-	}))
-
-it('installs existing packages with pnpm when pnpm was used to run the preset', async () =>
-	await usingSandbox({
-		fn: async ({ targetDirectory }, makeTestPreset) => {
-			process.env.npm_config_user_agent = 'pnpm'
-			const { executePreset } = await makeTestPreset({
-				handler: async () =>
-					await installPackages({
-						for: 'node',
-						additionalArgs: ['--ignore-workspace'],
-					}),
-			})
-
-			await executePreset()
-			await expectStructureMatches(targetDirectory, {
-				'node_modules': { type: 'directory' },
-				'node_modules/debug': { type: 'directory' },
-				'pnpm-lock.yaml': { type: 'file' },
 			})
 		},
 		targetStructure: {
